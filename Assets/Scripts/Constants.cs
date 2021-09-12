dd6b5fefe97909c69b226d9e67b769246fca9f17
wingsmight <wingsmight@gmail.com>
2021-09-12 15:08:59 +0700

    add comments for some constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,9 +1,9 @@
 public class Constants
 {
-    public const float APLHA = 1.0f;
-    public const float HI = 1.0f;
-    public const float BETA = 1.0f;
-    public const float RHO = 1.0f;
+    public const float APLHA = 1.0f; // reflection coefficient
+    public const float HI = 1.0f; // transmittance
+    public const float BETA = 1.0f; // optical return loss (ORL) coefficient
+    public const float RHO = 1.0f; // mirror reflectivity
     public const float ETA = 1.0f;
     public const float ZN = 1.0f;
     public const float E = 1.0f; // power of the light flow ot surface unit
